c661250e0af5b09b89879a937cee01297e7851f2
Umar Hussain <umar.hussain.da@outlook.com>
2022-02-01 10:41:39 +0000

    added class for sql alchemy engine - added argv to add in path

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from src.logger import logger_util
 from src.citations import flatten_json, citation_df, create_iso_folder, strip_title, get_newest_file
 from src import logkey
 from src.standards import standards_dataframe
+from src.engine import Engine
 import sys
 from pathlib import Path
+from sys import argv
 
 logger = logger_util(__name__)
 
 
+# eng = Engine()
+
 if __name__ == '__main__':
+
+    destination_path = argv[1]
+    if not Path(destination_path).exists():
+        logger.critical(f'{destination_path} does not exist!')
+        raise Exception(f"{destination_path} does not exist!")
+
+    logger.info(f'Destination path - {destination_path}')
+
     logger.info('starting program')
     file = Path(__file__).parent.joinpath('osha/output/articles.json')
     key = logkey()
@@ -16,26 +28,30 @@ if __name__ == '__main__':
 
     logger.info(f"Latest file - {file.stem}")
     df = flatten_json(file)
-    iso_path = create_iso_folder('osha/output')
 
     loi = strip_title(df)
     loi['Process'] = key
-    loi.to_csv(iso_path.joinpath('letters_of_interpretation.csv'), index=False)
+    loi.to_csv(Path(destination_path).joinpath('letters_of_interpretation.csv'), index=False)
     
 
-    logger.info('LOI written to iso folder')
+    logger.info('LOI written to stg folder')
 
     c_df = citation_df(df)
     c_df['Process'] = key
-    c_df.to_csv(iso_path.joinpath('citations.csv'), index=False)
-    logger.info('Citations written to iso folder')
+    c_df.to_csv(Path(destination_path).joinpath('citations.csv'), index=False)
+    logger.info('Citations written to stg folder')
 
-    standards_file =  Path(__file__).parent.joinpath('osha/output/standards.json')
+    standards_file =  Path(destination_path).joinpath('standards.json')
 
     logger.info(f"Latest file - {standards_file.stem}")
     standards_df = standards_dataframe(standards_file)
     standards_df['Process'] = key
-    standards_df.to_csv(iso_path.joinpath('standards.csv'), index=False)
-    
+    standards_df.to_csv(Path(destination_path).joinpath('LoiDocuments.csv'), index=False)
+    logger.info('Standards written to stg folder')
+
 
     
+
+
+
+
---
+++ src/engine.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine
+from dataclasses import dataclass
+
+import sqlalchemy
+
+
+@dataclass
+class Engine(sqlalchemy):
+
+    server: str
+    database: str
+    username: str
+    password: str
+
+    #post init#
+
+    def __post_init__(self):
+
+        object.__setattr__(self,'engine', 
+        self.create_engine(
+            f"mssql+pyodbc://{self.username}:{self.password}@{self.server}/{self.database}?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server",
+            fast_executemany=True)
+        )
+                        
+
+    def insert_batch_job(self,etl_name : str, target_file_name : str, src : str,
+                        destination_output : str) -> str:
+        """
+        generates the BatchID prior to starting ETL job - this will be used to update the log upon completion or failure.
+
+        """
+        sql_str = f"""
+            DECLARE @bid INT
+            
+            EXEC @bid = mc_datalayer.[etl].[sp_ins_batchProcess] 
+                @batchProcess = 'Merge Government Hosted Publicly Available Addative Content',
+                @BatchType = 'stg1 scrape',
+                @src =  {src}
+                @trf = {target_file_name},
+                @dst = {destination_output}
+        """	
+
+        with self.engine() as conn:
+            job_exec = conn.execute(sql_str)
+            jobkey = job_exec.first()[0]
+        return jobkey
+
+
+    def update_batch_job(self, bid):
+        """updates job table after completion"""
+
+        sql_str = f"""EXEC mc_datalayer.[etl].[sp_upd_batchProcess] @{bid}"""
+
+        with self.engine() as conn:
+
+            conn.execute(sql_str)
