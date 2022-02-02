a63ebba95f5183914c8cbbf6c1e373127a151a34
Umar Hussain <umar.hussain.da@outlook.com>
2022-02-02 21:57:10 +0000

    code re-work and adding engine updates for sql server

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from src.logger import logger_util
-from src.citations import flatten_json, citation_df, create_iso_folder, strip_title, get_newest_file
-from src import logkey
-from src.standards import standards_dataframe
-from src.engine import Engine
-import sys
 from pathlib import Path
 from sys import argv
 
-logger = logger_util(__name__)
+import pandas as pd
 
+from scrapy.crawler import CrawlerProcess
+from osha.spiders import oshaSpider, StandardRegsSpider
+
+
+from src import logkey
+from src.citations import (citation_df, create_iso_folder, flatten_json,
+                           get_newest_file, strip_title)
+from src.engine import Engine
+from src.logger import logger_util
+from src.standards import standards_dataframe
+
+
+logger = logger_util(__name__)
 
 eng = Engine()
 
 if __name__ == '__main__':
 
+    logger.info('starting articles/citations spider')
+
+
+    key = eng.insert_batch_job(batch_type='articles',destination_output='output/articles.json',
+                               src='https://www.osha.gov/laws-regs/standardinterpretations/publicationdate')
+
+    
+    #start process crawler and output to json
+
+    process = CrawlerProcess({'FEED_FORMAT': 'json','FEED_URI': 'output/articles.json'})
+    process.crawl(oshaSpider)
+    process.start()
+    row_count = pd.read_json('output/standards.json').shape[0]
+    eng.update_batch_job(bid=key, row_count=row_count)
+
+
+    process = CrawlerProcess({'FEED_FORMAT': 'json','FEED_URI': 'output/standards.json'})
+    key = eng.insert_batch_job(batch_type='articles',destination_output='output/articles.json',
+                               src='https://www.osha.gov/laws-regs/standardinterpretations/standards')
+    process.crawl(StandardRegsSpider)
+    process.start()                               
+
+    row_count = pd.read_json('output/standards.json').shape[0]
+    eng.update_batch_job(bid=key, row_count=row_count)
+
+
+
+    eng.insert_batch_job(batch_type='main.py - etl', destination_path='/stg1/', )
     destination_path = argv[1]
     if not Path(destination_path).exists():
         logger.critical(f'{destination_path} does not exist!')
@@ -22,7 +57,7 @@ if __name__ == '__main__':
     logger.info(f'Destination path - {destination_path}')
 
     logger.info('starting program')
-    file = Path(__file__).parent.joinpath('osha/output/articles.json')
+    file = Path(__file__).parent.joinpath('output/articles.json')
     key = logkey()
     logger.info(f'key is {key}')
 
@@ -41,18 +76,11 @@ if __name__ == '__main__':
     c_df.to_csv(Path(destination_path).joinpath('citations.csv'), index=False)
     logger.info('Citations written to stg folder')
 
-    standards_file =  Path(destination_path).joinpath('standards.json')
+    standards_file =  Path(__file__).joinpath('output/standards.json')
 
     logger.info(f"Latest file - {standards_file.stem}")
     standards_df = standards_dataframe(standards_file)
     standards_df['Process'] = key
     standards_df.to_csv(Path(destination_path).joinpath('LoiDocuments.csv'), index=False)
     logger.info('Standards written to stg folder')
-
-    
-
-
-
-
-
-
+    eng.update_batch_job(bid=key, row_count=0)
--- src/engine.py
+++ src/engine.py
@@ -32,22 +32,23 @@ class Engine:
 
                         
 
-    def insert_batch_job(self,etl_name : str, target_file_name : str, src : str,
-                        destination_output : str) -> str:
+    def insert_batch_job(self,batch_type : str, 
+                         target_file_name : str, src : str,
+                         destination_output : str) -> str:
         """
-        generates the BatchID prior to starting ETL job - this will be used to update the log upon completion or failure.
+        generates the BatchID prior to starting ETL job - 
+        this will be used to update the log upon completion or failure.
+        """
 
-        """
         sql_str = f"""
             DECLARE @bid INT
-            
             EXEC @bid = mc_datalayer.[etl].[sp_ins_batchProcess] 
                 @batchProcess = 'Merge Government Hosted Publicly Available Addative Content',
-                @BatchType = 'stg1 scrape',
+                @BatchType = '{batch_type}',
                 @src =  '{src}',
                 @trf = '{target_file_name}',
                 @dst = '{destination_output}'
-        """	
+        """
 
         with self.engine.begin() as conn:
             job_exec = conn.execute(sql_str)
@@ -55,11 +56,12 @@ class Engine:
         return pd.read_sql("select max(batchid) as bid from etl.batchprocess", self.engine)['bid'].values[0]
 
 
-    def update_batch_job(self, bid):
+    def update_batch_job(self, bid : str, row_count : str):
         """updates job table after completion"""
 
-        sql_str = f"""EXEC mc_datalayer.[etl].[sp_upd_batchProcess] @'{bid}' """
+        sql_str = f"""EXEC mc_datalayer.[etl].[sp_upd_batchProcess] '{bid}', '{row_count}' """
 
         with self.engine.begin() as conn:
-
             conn.execute(sql_str)
+
+
