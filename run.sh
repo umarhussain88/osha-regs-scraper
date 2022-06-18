ae2c0f7192f4c2365ba2cdf6137da715d934d948
umar-desktop <umar.hussain.da@outlook.com>
2022-06-18 17:25:13 +0100

    adding pipenv path

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # !/bin/sh
 
 export LC_ALL=en_US.utf-8
-pipenv run python3 main.py oshaSpider
-pipenv run python3 main.py StandardRegsSpider
-pipenv run python3 main.py phmsa_RegulationsSpider
-pipenv run python3 main.py clean-phmsa-regulations
-pipenv run python3 main.py export-azure
+./local/bin/pipenv run python3 main.py oshaSpider
+./local/bin/pipenv run python3 main.py StandardRegsSpider
+./local/bin/pipenv run python3 main.py phmsa_RegulationsSpider
+./local/bin/pipenv run python3 main.py clean-phmsa-regulations
+./local/bin/pipenv run python3 main.py export-azure
